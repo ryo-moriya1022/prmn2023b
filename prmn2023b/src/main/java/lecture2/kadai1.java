433dba98d3cb0e3d0cd7ca19dccb080f59eecce0
1 <[regu7828@gmail.com]>
2023-11-08 12:20:14 +0900

    lecture1

--- prmn2023b/src/main/java/lecture2/kadai1.java
+++ prmn2023b/src/main/java/lecture2/kadai1.java
@@ -1,2 +1,9 @@
-package lecture2;public class kadai1 {
+package lecture2;
+
+public class kadai1 {
+    public static void main(String[] args) {
+        String name1 = "千歳太郎";
+        int num = 2001000;
+        System.out.printf("氏名:%s 学籍番号:b%d", name1, num);
+    }
 }
--- prmn2023b/src/main/java/lecture2/kadai2.java
+++ prmn2023b/src/main/java/lecture2/kadai2.java
@@ -1,2 +1,37 @@
-package lecture2;public class kadai2 {
+package lecture2;
+import java.util.Scanner;
+
+public class kadai2 {
+    public static void main(String[] args) {
+        String name_1 = "千歳花子";
+        int num_1 = 2110000;
+        Scanner scan = new Scanner(System.in);
+        Login log = new Login(name_1, num_1);
+
+        System.out.print("名前を入力して下さい: ");
+        String name_2 = scan.next();
+        System.out.print("学籍番号を入力して下さい: ");
+        int num_2 = scan.nextInt();
+
+        String message = log.sys(name_2, num_2);
+        System.out.println(message);
+    }
+}
+
+class Login {
+    String name;
+    int num;
+
+    Login(String name, int num) {
+        this.name = name;
+        this.num = num;
+    }
+
+    String sys(String name_n, int num_n) {
+        if (name.equals(name_n) && num == num_n) {
+            return "ログイン成功。";
+        } else {
+            return "ログイン失敗";
+        }
+    }
 }
